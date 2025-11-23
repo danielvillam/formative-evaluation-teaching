52facbb7344a6f26132252be9780ac5b5e70df88
danielvillam <jvillame@unal.edu.co>
2025-11-22 22:16:34 -0500

    refactor: simplify MongoDB client connection logic and ensure proper connection handling

--- api/db.js
+++ api/db.js
@@ -1,27 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-const options = {};
-
-let client;
-let clientPromise;
-
-if (!uri) {
-  throw new Error('Please add your MongoDB URI to .env.local');
-}
-
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+const client = new MongoClient(process.env.MONGODB_URI);
 
 export async function connectToDatabase() {
-  const client = await clientPromise;
+  if (!client.isConnected) {
+    await client.connect();
+  }
   return client.db('evaluation_formative');
 }
